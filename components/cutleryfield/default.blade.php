51fa6158c6f50fab5f0e564a8111f99d17dbd9a6
Ryan Mitchell <ryan@thoughtcollective.com>
2021-02-22 19:59:41 +0000

    Doh

--- components/cutleryfield/default.blade.php
+++ components/cutleryfield/default.blade.php
@@ -4,13 +4,13 @@
                 <label for="">Cutlery Required?</label><br>
                 <div class="list-group">
                         <div class="list-group-item bg-light">
-                                <div class="custom-control custom-radio" data-checkout-control="choose-payment" data-payment-code="cod">
+                                <div class="custom-control custom-radio">
                                     <input type="radio" id="cutlery-required-no" class="custom-control-input" name="cutlery_required" value="0" checked="checked" autocomplete="off">
                                     <label class="custom-control-label d-block" for="cutlery-required-no">No</label>
                                 </div>
                         </div>
                         <div class="list-group-item">
-                                <div class="custom-control custom-radio" data-checkout-control="choose-payment" data-payment-code="cod">
+                                <div class="custom-control custom-radio">
                                     <input type="radio" id="cutlery-required-yes" class="custom-control-input" name="cutlery_required" value="1" autocomplete="off">
                                     <label class="custom-control-label d-block" for="cutlery-required-yes">Yes</label>
                                 </div>
